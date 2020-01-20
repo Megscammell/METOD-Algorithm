c897c558abd30689e6eca03e61366f326babe75b
Megscammell <46384250+Megscammell@users.noreply.github.com>
2020-01-20 18:22:59 +0000

    Update test_warm_up_iterations.py

--- tests/test_warm_up_iterations.py
+++ tests/test_warm_up_iterations.py
@@ -21,7 +21,8 @@ def test_1():
 @settings(deadline=None)
 @given(st.integers(2,20), st.integers(2,100))
 def test_2(p, d):
-    """Check that apply_sd_until_warm_up produces points and corresponding partner points with m - 1 and m iterations of steepest descent applied
+    """Check that apply_sd_until_warm_up produces points and corresponding partner points with m - 1 
+       and m iterations of steepest descent applied
     """
     beta = 0.099
     tolerance = 0.00001
@@ -42,11 +43,11 @@ def test_2(p, d):
     #Generate random starting point
     x = np.random.uniform(0,1,(d,))
     #Apply one iteration of steepest descent
-    x_1, change_point = mtv3.sd_iteration(x, projection, option, met,                                                initial_guess, func_args, f, g)
+    x_1, change_point = mtv3.sd_iteration(x, projection, option, met, initial_guess, func_args, f, g)
     #Apply second iteration of steepest descent
-    x_2, change_point = mtv3.sd_iteration(x_1, projection, option, met,                                              initial_guess, func_args, f, g)
+    x_2, change_point = mtv3.sd_iteration(x_1, projection, option, met, initial_guess, func_args, f, g)
     #Apply third iteration of steepest descent
-    x_3, change_point = mtv3.sd_iteration(x_2, projection, option, met,                                              initial_guess, func_args, f, g)
+    x_3, change_point = mtv3.sd_iteration(x_2, projection, option, met, initial_guess, func_args, f, g)
     #Compute corresponding partner point for x_2
     z_2 = mtv3.partner_point(x_2, beta, d, g, func_args)
     #Compute corresponding partner point for x_3
@@ -62,7 +63,9 @@ def test_2(p, d):
 @settings(max_examples=10, deadline=None)
 @given(st.integers(2,20), st.integers(0,5), st.integers(2,100))
 def test_3(p, m, d):
-    """Check that continued iterations from x_2 to a minimizer, (iterations_of_sd_part) joined with the initial warm up points (warm_up_sd) has the same points and shape as when initial point x has steepest descent iterations applied to find the minimizer (iterations_of_sd_test). 
+    """Check that continued iterations from x_2 to a minimizer, (iterations_of_sd_part) joined with the 
+       initial warm up points (warm_up_sd) has the same points and shape as when initial point x has 
+       steepest descent iterations applied to find the minimizer (iterations_of_sd_test). 
     """
     beta = 0.099
     tolerance = 0.00001
