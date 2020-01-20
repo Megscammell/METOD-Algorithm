6acdccdbcd499f7843da78ec90e84193b1f4ff92
Megscammell <46384250+Megscammell@users.noreply.github.com>
2020-01-20 18:17:04 +0000

    Update test_check_iterations_line_search_option.py

--- tests/test_check_iterations_line_search_option.py
+++ tests/test_check_iterations_line_search_option.py
@@ -4,7 +4,10 @@ import metod_testing as mtv3
 
 
 def test_1():
-    """Particular case for line_search where solver doesn't converge. Will produce a warning message but want to check that the point is replaced and iterations are started again from the new point. Checks that the starting point is different to that in sd_iterations[0,:]. Also checks that sd_iterations[its,:] is the minima corresponding to sd_iterations[0,:].
+    """Particular case for line_search where solver doesn't converge. Will produce a warning message but want to 
+    check that the point is replaced and iterations are started again from the new point. Checks that the starting 
+    point is different to that in sd_iterations[0,:]. Also checks that sd_iterations[its,:] is the minima corresponding 
+    to sd_iterations[0,:].
     """    
     d = 20
     p = 10
@@ -22,10 +25,10 @@ def test_1():
     func_args = p, store_x0, matrix_test
     x = np.random.uniform(0,1,(d,))
     initial_point = True
-    sd_iterations, its = mtv3.apply_sd_until_stopping_criteria                                      (initial_point, x, d, projection,                                           tolerance, option, met, initial_guess,                                     func_args, f, g)
+    sd_iterations, its = mtv3.apply_sd_until_stopping_criteria(initial_point, x, d, projection, tolerance, option, met, initial_guess, func_args, f, g)
     assert(np.any(x != sd_iterations[0,:]))
 
     assert(mtv3.calc_pos(x, *func_args)[0] != mtv3.calc_pos(sd_iterations[its,:], *func_args)[0])
 
-    assert(mtv3.calc_pos(sd_iterations[0,:], *func_args)[0] == mtv3.calc_pos                                                              (sd_iterations                                                              [its, :],                                                                  *func_args)[0])
+    assert(mtv3.calc_pos(sd_iterations[0,:], *func_args)[0] == mtv3.calc_pos(sd_iterations[its, :], *func_args)[0])
 
