48a8480a320411924f37fb8d002ca296ae71eff1
Megscammell <46384250+Megscammell@users.noreply.github.com>
2020-01-20 18:01:12 +0000

    Update distances.py

--- src/metod_testing/distances.py
+++ src/metod_testing/distances.py
@@ -5,9 +5,9 @@ def distances(set_of_points, point, set_of_points_num, d):
     """Compute Euclidean distances for each point in set_of_points with single_point
 
     Keyword arguments:
-    set_of_points -- is an array of size N x d containing all iterations of                     steepest descent of a point
+    set_of_points -- is an array of size N x d containing all iterations of steepest descent of a point
     point -- is a (d,) or d x 1 array
-    set_of_points_num -- the row number of set_of_points to start computing                         Euclidean distance with point
+    set_of_points_num -- the row number of set_of_points to start computing Euclidean distance with point
     d -- is dimension
     """
 
