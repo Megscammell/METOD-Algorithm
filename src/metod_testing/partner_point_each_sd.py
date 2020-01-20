7b435ee460119fd940593f52b27f18216c945dbc
Megscammell <46384250+Megscammell@users.noreply.github.com>
2020-01-20 18:11:14 +0000

    Update partner_point_each_sd.py

--- src/metod_testing/partner_point_each_sd.py
+++ src/metod_testing/partner_point_each_sd.py
@@ -3,16 +3,16 @@ import numpy as np
 import metod_testing as mtv3
 
 
-def partner_point_each_sd(all_iterations_of_sd, d, beta, iterations, g,                                  func_args):
+def partner_point_each_sd(all_iterations_of_sd, d, beta, iterations, g, func_args):
     """Compute all corresponding partner points for each iteration of steepest descent for point
 
     Keyword arguments:
-    all_iterations_of_sd -- is an array of size (iterations + 1) x d                                   containing all iterations of steepest descent of a                         point
+    all_iterations_of_sd -- is an array of size (iterations + 1) x d containing all iterations of steepest descent of a point
     d -- is dimension
     beta -- fixed small step size 
     iterations -- total number of steepest descent iterations
     g -- user defined gradient
-    func_args -- parameters needed to compute the function and                              gradient
+    func_args -- parameters needed to compute the function and gradient
     """
     all_iterations_of_sd_partner_points = np.zeros((iterations + 1, d))
     for its in range(iterations + 1):
