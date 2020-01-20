9264f9e7eae3e5f1232364f1361d54cfee12cac5
Megscammell <46384250+Megscammell@users.noreply.github.com>
2020-01-20 18:08:05 +0000

    Update metod_indepth.py

--- src/metod_testing/metod_indepth.py
+++ src/metod_testing/metod_indepth.py
@@ -27,11 +27,11 @@ def metod_indepth(f, g, func_args, d, num_points = 1000, beta = 0.01,
     num_points -- number of uniform random points generated
     beta -- small constant step size to compute partner points
     tolerance -- stopping condition for steepest descent iterations
-    projection -- is a boolean variable. If projection = True, this projects                 points back to the [0,1]^d cube.
-    const --  a constant for the minimum euclidean distance to be larger than            to classify a point as a new local minima
+    projection -- is a boolean variable. If projection = True, this projects points back to the [0,1]^d cube.
+    const --  a constant for the minimum euclidean distance to be larger than to classify a point as a new local minima
     m -- warm up period
-    initial_guess -- is passed to the scipy.optimize.minimize                                   function. This is recommended to be small (0.05). Method                   chosen is Nelder-Mead.
-    method_min -- Choose from 'Nelder-Mead', 'CG', 'BFGS', 'L-BFGS-B', 'TNC',               'COBYLA', 'SLSQP', 'trust-constr'
+    initial_guess -- is passed to the scipy.optimize.minimize function. This is recommended to be small (0.05). Method chosen is Nelder-Mead.
+    method_min -- Choose from 'Nelder-Mead', 'CG', 'BFGS', 'L-BFGS-B', 'TNC', 'COBYLA', 'SLSQP', 'trust-constr'
     """
     if isinstance(d, int) == False:
         raise ValueError('d must be an integer.')
@@ -79,48 +79,48 @@ def metod_indepth(f, g, func_args, d, num_points = 1000, beta = 0.01,
     x = np.random.uniform(0, 1, (d,))
     initial_point = True
     iterations_of_sd, its = mtv3.apply_sd_until_stopping_criteria(
-                                        initial_point, x, d, projection, tolerance, option, met, initial_guess, func_args, f, g)
+                            initial_point, x, d, projection, tolerance, option, met, initial_guess, func_args, f, g)
     starting_points[0,:] = iterations_of_sd[0,:].reshape(1,d)
     store_its.append(its)
     des_x_points.append(iterations_of_sd)
     discovered_minimas.append(iterations_of_sd[its].reshape(d,))
-    sd_iterations_partner_points = mtv3.partner_point_each_sd                                                 (iterations_of_sd, d, beta, its,                                            g, func_args)
+    sd_iterations_partner_points = mtv3.partner_point_each_sd(iterations_of_sd, d, beta, its, g, func_args)
     des_z_points.append(sd_iterations_partner_points)
     number_minimas = 1
     for remaining_points in tqdm.tqdm(range(num_points - 1)):
         initial_point = False
         x = np.random.uniform(0, 1, (d,))
-        warm_up_sd, warm_up_sd_partner_points = mtv3.apply_sd_until_warm_up (x,                                         d, m, beta,projection,option,                                          met, initial_guess,func_args,                                          f, g)
+        warm_up_sd, warm_up_sd_partner_points = mtv3.apply_sd_until_warm_up (x,d, m, beta,projection,option, met, initial_guess,func_args, f, g)
         
         x_1 = warm_up_sd[m - 1].reshape(d,)
         z_1 = warm_up_sd_partner_points[m - 1].reshape(d,)
         x_2 = warm_up_sd[m].reshape(d,)
         z_2 = warm_up_sd_partner_points[m].reshape(d,) 
    
-        possible_regions = mtv3.check_alg_cond(number_minimas, x_1, z_1,                                              x_2, z_2, des_x_points,                                                des_z_points, m - 1, d)
+        possible_regions = mtv3.check_alg_cond(number_minimas, x_1, z_1, x_2, z_2, des_x_points, des_z_points, m - 1, d)
 
         if possible_regions == []:
             iterations_of_sd_part, its = mtv3.apply_sd_until_stopping_criteria(initial_point, x_2, d, projection, tolerance, option, met, initial_guess, func_args, f, g)
 
-            iterations_of_sd = np.vstack([warm_up_sd,                                                       iterations_of_sd_part[1:,]])
+            iterations_of_sd = np.vstack([warm_up_sd, iterations_of_sd_part[1:,]])
             des_x_points.append(iterations_of_sd)
-            starting_points[remaining_points + 1,:] = iterations_of_sd[0,:]                                              .reshape(1,d)
+            starting_points[remaining_points + 1,:] = iterations_of_sd[0,:].reshape(1,d)
 
             discovered_minimas.append(iterations_of_sd[its + m].reshape(d,))
 
             store_its.append(its + m)
 
-            sd_iterations_partner_points_part = mtv3.partner_point_each_sd                                         (iterations_of_sd_part, d,                                          beta, its, g, func_args)
-            sd_iterations_partner_points = np.vstack(                                                         [warm_up_sd_partner_points,                                         sd_iterations_partner_points_part                                  [1:,]])
+            sd_iterations_partner_points_part = mtv3.partner_point_each_sd(iterations_of_sd_part, d, beta, its, g, func_args)
+            sd_iterations_partner_points = np.vstack([warm_up_sd_partner_points, sd_iterations_partner_points_part[1:,]])
 
             des_z_points.append(sd_iterations_partner_points)
             number_minimas += 1
         else:
             starting_points[remaining_points + 1,:] = warm_up_sd[0].reshape(1,d)
 
-    unique_minimas, unique_number_of_minima = mtv3.check_unique_minimas                                                  (discovered_minimas, const)
-    func_vals_of_minimas = [f(element, *func_args) for element in                                      unique_minimas]
+    unique_minimas, unique_number_of_minima = mtv3.check_unique_minimas(discovered_minimas, const)
+    func_vals_of_minimas = [f(element, *func_args) for element in unique_minimas]
                     
     
-    return unique_minimas, unique_number_of_minima, func_vals_of_minimas, (len        (des_x_points) - unique_number_of_minima), store_its, des_x_points,        des_z_points, starting_points
+    return unique_minimas, unique_number_of_minima, func_vals_of_minimas, (len (des_x_points) - unique_number_of_minima), store_its, des_x_points, des_z_points, starting_points
 
