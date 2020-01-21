470396345b0eb65658ea91f336a7e2b58adaab8c
Megscammell <46384250+Megscammell@users.noreply.github.com>
2020-01-21 13:58:12 +0000

    Update main.py

--- src/metod_testing/main.py
+++ src/metod_testing/main.py
@@ -7,14 +7,16 @@ f = mtv3.quad_function
 g = mtv3.quad_gradient
 d = 20 
 
-#2) Will need to amend/add/delete the below if function and gradient is not the minimum of several quadratic forms. Otherwise, can update values for the below parameters
+#2) Will need to amend/add/delete the below if function and gradient is not the minimum of several quadratic forms. 
+#Otherwise, can update values for the below parameters
 P = 5
 lambda_1 = 1
 lambda_2 = 10
 store_x0, matrix_combined = mtv3.function_parameters_quad(P, d, lambda_1, lambda_2)
 args = P, store_x0, matrix_combined
 
-#3) Run METOD algorithm. If any of the optional input parameters need to be updated this will need to be passed to mtv3.metod(f, g, args, d).
+#3) Run METOD algorithm. If any of the optional input parameters need to be updated this will need to be passed 
+#to mtv3.metod(f, g, args, d).
 discovered_minima, number_minima, func_vals_of_minima, excessive_no_descents  = mtv3.metod(f, g, args, d)
 np.savetxt('discovered_minimas_d_%s_p_%s.csv' % (d, P), discovered_minima, delimiter=",")
 
