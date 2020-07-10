b66d0cdb7b0aea66af8240dc7bf73ca35b957951
Meg Scammell <scammellm@cardiff.ac.uk>
2020-07-10 14:26:36 +0100

    Update format and comments of calc_minima.py

--- src/metod_testing/calc_minima.py
+++ src/metod_testing/calc_minima.py
@@ -2,14 +2,31 @@ import numpy as np
 from numpy import linalg as LA
 
 
-def calc_minima(point, p, exp_const, store_x0, matrix_test, store_c):
-    """ Finds the nearest local minima for x_n^(K_n) for the sum of Gaussians
+def calc_minima(point, p, sigma_sq, store_x0, matrix_test, store_c):
+    """ Finds the nearest local minima for point using the Sum of Gaussians
      function
 
-    Keyword arguments:
-    point -- is a (d,) array
-    p, exp_const, store_x0, matrix_test, store_c -- function arguments for the
-     sum of Gaussians functions.
+    Parameters
+    ----------
+    point : 1-D array with shape (d, )
+            A point used to evaluate the function.
+    p : integer
+        Number of local minima.
+    sigma_sq: float or integer
+              Value of sigma squared.
+    store_x0 : 2-D arrays with shape (p, d).
+    matrix_test : 3-D arrays with shape (p, d, d).
+    store_c : 3-D arrays with shape (p, ).
+
+    Returns
+    -------
+    np.argmin(dist) : integer
+                      Position of the local minima which produces the
+                      smallest distance between point and all p local
+                      minimas.
+    np.min(dist) : float
+                   The smallest distance between point and all p local
+                   minimas
     """
     dist = np.zeros((p))
     for i in range(p):
