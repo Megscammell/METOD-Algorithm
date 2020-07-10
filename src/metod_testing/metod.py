23bb31be4396b7b21f916e408ade1e44270ea5fa
Meg Scammell <scammellm@cardiff.ac.uk>
2020-07-10 14:22:30 +0100

    Update format and comments in metod.py

--- src/metod_testing/metod.py
+++ src/metod_testing/metod.py
@@ -1,66 +1,128 @@
 from warnings import warn
-
 import numpy as np
-import tqdm
 
 import metod_testing as mtv3
 
 
 def metod(f, g, func_args, d, num_points=1000, beta=0.01,
-          tolerance=0.00001, projection=False, const=0.1,
-          m=3, option='minimize', met='Nelder-Mead', initial_guess=0.05,
+          tolerance=0.00001, projection=False, const=0.1, m=3,
+          option='minimize', met='Nelder-Mead', initial_guess=0.05,
           set_x=np.random.uniform, bounds_set_x=(0, 1)):
-    """Returns total number of minima discovered by algorithm, position of
-     local minima, function values of local minima and number of unnecessary
-      descents.
+    """Apply METOD algorithm with specified parameters.
 
-    Keyword arguments:
-    f -- function
-    g -- gradient
-    func_args -- arguments passed to f and g
-    d -- is the dimension
-    num_points -- number of uniform random points generated
-    beta -- small constant step size to compute partner points
-    tolerance -- stopping condition for steepest descent iterations
-    projection -- is a boolean variable. If projection = True, this projects
-     points back to bounds_set_x.
-    const --  a constant for the minimum euclidean distance to be larger than
-     to classify a point as a new local minima
-    m -- warm up period
-    option - choose from 'minimize' or 'minimize_scalar'. Default is
-     'minimize'.
-    met -- Choose appropiate method for option. Default is 'Nelder-Mead'.
-    initial_guess -- is passed to the scipy.optimize.minimize function. This
-     is recommended to be small (0.05). Method chosen is Nelder-Mead.
-    set_x -- Generates points for the METOD algorithm. Can be either inbuilt
-     numpy.random distribution that generates points, list or numpy array of
-      size num_points
-    bounds_set_x - Bounds for numpy.random distribution
+    Parameters
+    ----------
+    f : objective function.
+
+        ``f(x, *func_args) -> float``
+
+        where ``x`` is a 1-D array with shape(d, ) and func_args is a
+        tuple of arguments needed to compute the function value.
+    g : gradient of objective function.
+
+       ``g(x, *func_args) -> 1-D array with shape (d, )``
+
+        where ``x`` is a 1-D array with shape(d, ) and func_args is a
+        tuple of arguments needed to compute the gradient.
+    func_args : tuple
+                Arguments passed to f and g.
+    d : integer
+        Size of dimension.
+    num_points : integer (optional)
+                 Number of random points generated. The Default is
+                 num_points=1000.
+    beta : float or integer (optional)
+           Small constant step size to compute the partner points.
+           The Default is beta=0.01.
+    tolerance : float (optional)
+                Stopping condition for steepest descent iterations.
+                The Default is tolerance=0.00001.
+    projection : boolean (optional)
+                 If projection is True, this projects points back to
+                 bounds_set_x. If projection is False, points are
+                 kept the same. The Default is projection=False.
+    const : float or integer (optional)
+            In order to classify point x as a new local minima, the
+            euclidean distance between x and all other discovered local
+            minima must be larger than const. The Default is const=0.1.
+    m : integer (optional)
+        Number of iterations of steepest descent to apply to point
+        x before making decision on terminating descents. The Default
+        is m=3.
+    option : string (optional)
+             Choose from 'minimize' or 'minimize_scalar'. For more
+             information about each option see
+             https://docs.scipy.org/doc/scipy/reference/optimize.html.
+             Default is 'minimize'.
+    met : string (optional)
+         Choose method for option. For more information see
+         - https://docs.scipy.org/doc/scipy/reference/generated/
+         scipy.optimize.minimize.html#scipy.optimize.minimize
+         - https://docs.scipy.org/doc/scipy/reference/generated/
+         scipy.optimize.minimize_scalar.html#scipy.optimize.minimize_scalar
+         Default is 'Nelder-Mead'.
+    initial_guess : float or integer (optional)
+                    Initial guess passed to scipy.optimize.minimize. This
+                    is recommended to be small. The default is
+                    initial_guess=0.05.
+    set_x : numpy.random distribution, list or np.ndarray (optional)
+            If numpy.random distribution is selected, generates random
+            starting points for the METOD algorithm. If list or a numpy
+            array of size num_points is passed, then the METOD algorithm
+            uses these points as staring points. The Default is
+            set_x=np.random.uniform.
+    bounds_set_x : tuple (optional)
+                   Bounds for numpy.random distribution. The Default is
+                   bounds_set_x=(0, 1).
+
+    Returns
+    -------
+    unique_minima : list
+                    Contains all 1-D arrays with shape (d, ) of
+                    unique minima.
+    unique_number_of_minima: integer
+                             Total number of unique minima found (L).
+    func_vals_of_minimas : list
+                           Function values at each unique minima.
+    (len(des_x_points) - unique_number_of_minima)): integer
+                                                    Number of excessive
+                                                    descents.
+
     """
-    if isinstance(d, int) is False:
+    if type(d) is not int:
         raise ValueError('d must be an integer.')
-    if isinstance(num_points, int) is False:
+    if type(num_points) is not int:
         raise ValueError('num_points must be an integer.')
-    if isinstance(beta, float) is False:
-        raise ValueError('beta must be a float.')
-    if isinstance(tolerance, float) is False:
+    if (type(beta) is not int) and (type(beta) is not float):
+        raise ValueError('beta must be an integer or float.')
+    if type(tolerance) is not float:
         raise ValueError('tolerance must be a float.')
-    if isinstance(projection, bool) is False:
+    if type(projection) is not bool:
         raise ValueError('projection must be boolean.')
-    if isinstance(const, float) is False:
-        raise ValueError('const must be a float.')
-    if isinstance(m, int) is False:
+    if (type(const) is not int) and (type(const) is not float):
+        raise ValueError('const must be an integer or float.')
+    if type(m) is not int:
         raise ValueError('m must be an integer.')
-    if isinstance(option, str) is False:
+    if type(option) is not str:
         raise ValueError('option must be a string.')
-    if isinstance(met, str) is False:
+    if type(met) is not str:
         raise ValueError('met must be a string.')
-    if isinstance(initial_guess, float) is False:
+    if (type(initial_guess) is not int) and (type(initial_guess) is not float):
         raise ValueError('initial_guess must be a float.')
+    if ((type(bounds_set_x[0]) is not int) and (type(bounds_set_x[0]) is not
+                                                float)):
+        raise ValueError('bounds_set_x does not contain bounds which are'
+                         ' floats or integers .')
+    if ((type(bounds_set_x[1]) is not int) and (type(bounds_set_x[1]) is not
+                                                float)):
+        raise ValueError('bounds_set_x does not contain bounds which are'
+                         ' floats or integers .')
     if d < 2:
         raise ValueError('must have d > 1')
     if m < 1:
         raise ValueError('must have m >= 1')
+    if len(bounds_set_x) != 2:
+        raise ValueError('length of bounds_set_x is less or greater than 2')
     if type(set_x) is list or type(set_x) is np.ndarray:
         num_points = len(set_x)
         projection = False
@@ -77,11 +139,9 @@ def metod(f, g, func_args, d, num_points=1000, beta=0.01,
         warn('Tolerance is too high and replaced with default.',
              RuntimeWarning)
         tolerance = 0.00001
-
     des_x_points = []
     des_z_points = []
     discovered_minimas = []
-
     if type(set_x) is list or type(set_x) is np.ndarray:
         x = set_x[0]
         if x.shape[0] != d:
@@ -89,7 +149,6 @@ def metod(f, g, func_args, d, num_points=1000, beta=0.01,
                              ' d')
     else:
         x = set_x(*bounds_set_x, (d, ))
-
     iterations_of_sd, its = mtv3.apply_sd_until_stopping_criteria(
                             x, d, projection, tolerance, option, met,
                             initial_guess, func_args, f, g, bound_1, bound_2)
@@ -99,16 +158,14 @@ def metod(f, g, func_args, d, num_points=1000, beta=0.01,
                          'Please change m or change tolerance.')
     des_x_points.append(iterations_of_sd)
     discovered_minimas.append(iterations_of_sd[its].reshape(d,))
-
     sd_iterations_partner_points = (mtv3.partner_point_each_sd
                                     (iterations_of_sd, d, beta, its, g,
                                      func_args))
-
     des_z_points.append(sd_iterations_partner_points)
     number_minimas = 1
-    for remaining_points in tqdm.tqdm(range(num_points - 1)):
+    for remaining_points in range(num_points - 1):
         if type(set_x) is list or type(set_x) is np.ndarray:
-            x = set_x[remaining_points + 1, :]
+            x = set_x[remaining_points + 1]
             if x.shape[0] != d:
                 raise ValueError('set_x does not contain points which are of '
                                  'size d')
@@ -131,15 +188,10 @@ def metod(f, g, func_args, d, num_points=1000, beta=0.01,
                                           (x_2, d, projection, tolerance,
                                            option, met, initial_guess,
                                            func_args, f, g, bound_1, bound_2))
-            if (its + m) <= m:
-                raise ValueError('m is equal to or larger than the total '
-                                 'number of steepest descent iterations to '
-                                 'find a minimizer. Please change m or change '
-                                 'tolerance.')
             iterations_of_sd = np.vstack([warm_up_sd,
                                           iterations_of_sd_part[1:, ]])
             des_x_points.append(iterations_of_sd)
-            discovered_minimas.append(iterations_of_sd[its+m].reshape(d,))
+            discovered_minimas.append(iterations_of_sd[its + m].reshape(d, ))
             sd_iterations_partner_points = (mtv3.partner_point_each_sd
                                             (iterations_of_sd, d, beta, its +
                                              m, g, func_args))
--- tests/test_metod.py
+++ tests/test_metod.py
@@ -1,38 +1,232 @@
 import numpy as np
+import pytest
 from hypothesis import assume, given, settings, strategies as st
 
 import metod_testing as mtv3
 
 
+def func_params(d=20, p=2, lambda_1=1, lambda_2=10):
+    """Generates parameters to use for tests 1 - 20"""
+    f = mtv3.quad_function
+    g = mtv3.quad_gradient
+    store_x0, matrix_test = mtv3.function_parameters_quad(p, d, lambda_1,
+                                                          lambda_2)
+    func_args = p, store_x0, matrix_test
+    return f, g, func_args
+
+
 def test_1():
-    """Test that all parameters are checked before being applied in algorithm.
+    """Asserts error message when num_points is not integer"""
+    d = 20
+    f, g, func_args = func_params()
+    num_points_t = 0.01
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, num_points=num_points_t)
+
+
+def test_2():
+    """Asserts error message when d is not integer"""
+    d = 0.01
+    p = 10
+    f = mtv3.quad_function
+    g = mtv3.quad_gradient
+    func_args = (p, np.random.uniform(0, 1, (p, )),
+                 np.random.uniform(0, 1, (p, 10, 10)))
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d)
+
+
+def test_3():
+    """Asserts error message when beta is not integer or float"""
+    d = 20
+    f, g, func_args = func_params()
+    beta_t = True
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, beta=beta_t)
+
+
+def test_4():
+    """Asserts error message when tolerance is not float"""
+    d = 20
+    f, g, func_args = func_params()
+    tolerance_t = True
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, tolerance=tolerance_t)
+
+
+def test_5():
+    """Asserts error message when projection is not boolean"""
+    d = 20
+    f, g, func_args = func_params()
+    projection_t = 0.01
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, projection=projection_t)
+
+
+def test_6():
+    """Asserts error message when const is not integer or float"""
+    d = 20
+    f, g, func_args = func_params()
+    const_t = 'test'
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, const=const_t)
+
+
+def test_7():
+    """Asserts error message when m is not integer"""
+    d = 20
+    f, g, func_args = func_params()
+    m_t = 0.9
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, m=m_t)
+
+
+def test_8():
+    """Asserts error message when option is not a string"""
+    d = 20
+    f, g, func_args = func_params()
+    option_t = True
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, option=option_t)
+
+
+def test_9():
+    """Asserts error message when met is not a string"""
+    d = 20
+    f, g, func_args = func_params()
+    met_t = 0.1
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, met=met_t)
+
+
+def test_10():
+    """Asserts error message when initial_guess is not a integer or float"""
+    d = 20
+    f, g, func_args = func_params()
+    initial_guess_t = '213'
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d,
+                   initial_guess=initial_guess_t)
+
+
+def test_11():
+    """Asserts error message when d < 2"""
+    d = 1
+    p = 10
+    f = mtv3.quad_function
+    g = mtv3.quad_gradient
+    func_args = (p, np.random.uniform(0, 1, (p, )),
+                 np.random.uniform(0, 1, (p, 10, 10)))
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d)
+
+
+def test_12():
+    """Asserts error message when m < 1"""
+    d = 20
+    f, g, func_args = func_params()
+    m_t = 0
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, m=m_t)
+
+
+def test_13():
+    """Asserts error message when dimension of set_x not the same as d"""
+    d = 20
+    f, g, func_args = func_params()
+    set_x_test = np.random.uniform(0, 1, (50, 10))
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, set_x=set_x_test)
+
+
+def test_14():
     """
-    num_points = 0.001
-    ans_1 = True
-    if isinstance(num_points, int) is False:
-        ans_1 = False
-    assert(ans_1 is False)
-    num_points = 100
-    ans_2 = True
-    if isinstance(num_points, int) is False:
-        ans_2 = False
-    assert(ans_2 is True)
-    proj = 0.999
-    ans_3 = True
-    if isinstance(proj, bool) is False:
-        ans_3 = False
-    assert(ans_3 is False)
-    proj = True
-    ans_4 = True
-    if isinstance(proj, bool) is False:
-        ans_4 = False
-    assert(ans_4 is True)
+    Asserts error message when bounds_set_x do not contain integers or
+    floats
+    """
+    d = 20
+    f, g, func_args = func_params()
+    bounds_set_x_t = (True, 1)
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d,
+                   bounds_set_x=bounds_set_x_t)
+
+
+def test_15():
+    """
+    Asserts error message when bounds_set_x do not contain integers or
+    floats
+    """
+    d = 20
+    f, g, func_args = func_params()
+    bounds_set_x_t = (0, 'False')
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d,
+                   bounds_set_x=bounds_set_x_t)
+
+
+def test_16():
+    """Asserts warning message when beta >= 1"""
+    d = 20
+    f, g, func_args = func_params()
+    beta_t = 1
+    with pytest.warns(RuntimeWarning):
+        mtv3.metod(f, g, func_args, d, beta=beta_t)
+
+
+def test_17():
+    """Asserts warning message when tolerance > 0.1"""
+    d = 20
+    f, g, func_args = func_params()
+    tolerance_t = 0.2
+    with pytest.warns(RuntimeWarning):
+        mtv3.metod(f, g, func_args, d, tolerance=tolerance_t)
+
+
+def test_18():
+    """
+    Asserts error message when set_x does not contain points of same d
+    """
+    d = 20
+    f, g, func_args = func_params()
+    set_x_test = []
+    set_x_test.append(np.random.uniform(0, 1, (20, )))
+    set_x_test.append(np.random.uniform(0, 1, (20, )))
+    set_x_test.append(np.random.uniform(0, 1, (18, )))
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, set_x=set_x_test)
+
+
+def test_19():
+    """
+    Asserts error message when number of iterations is less than m
+    """
+    np.random.seed(90)
+    d = 2
+    p = 2
+    lambda_1 = 1
+    lambda_2 = 3
+    tolerance_t = 0.1
+    m_t = 6
+    f, g, func_args = func_params(d, p, lambda_1, lambda_2)
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d, tolerance=tolerance_t, m=m_t)
+
+
+def test_20():
+    """Asserts error message when len(bounds_set_x) > 2"""
+    d = 20
+    f, g, func_args = func_params()
+    bounds_set_x_t = (0, 1, 2)
+    with pytest.raises(ValueError):
+        mtv3.metod(f, g, func_args, d,
+                   bounds_set_x=bounds_set_x_t)
 
 
 @settings(max_examples=10, deadline=None)
 @given(st.integers(2, 20), st.integers(0, 3), st.integers(2, 100))
-def test_2(p, m, d):
-    """ Test warm up, m, is being applied correctly in metod when computing
+def test_21(p, m, d):
+    """ Test m is being applied correctly in metod.py when computing
      distances """
     np.random.seed(p)
     x = np.random.uniform(0, 1, (d, ))
@@ -57,8 +251,8 @@ def test_2(p, m, d):
     g = mtv3.quad_gradient
     iterations_of_sd, its = (mtv3.apply_sd_until_stopping_criteria
                              (x, d, projection, tolerance, option, met,
-                              initial_guess, func_args, f, g, bound_1=1,
-                              bound_2=0))
+                              initial_guess, func_args, f, g, bound_1=0,
+                              bound_2=1))
     """METOD algorithm checks the below"""
     assume(its > m)
     sd_iterations_partner_points = (mtv3.partner_point_each_sd
@@ -66,7 +260,6 @@ def test_2(p, m, d):
                                      func_args))
     test_x = np.random.uniform(0, 1, (d, ))
     original_shape = iterations_of_sd.shape[0]
-    test_x = np.random.uniform(0, 1, (d, ))
     """Checking correct warm up applied when checking distances"""
     set_dist = mtv3.distances(iterations_of_sd, test_x, m, d)
     assert(set_dist.shape == (original_shape - m,))
@@ -76,8 +269,8 @@ def test_2(p, m, d):
 
 @settings(max_examples=10, deadline=None)
 @given(st.integers(2, 20), st.integers(5, 100), st.integers(50, 1000))
-def test_3(p, d, num_points_t):
-    """Check ouputs of algorithm with minimum of several quadratic forms
+def test_22(p, d, num_points_t):
+    """Check ouputs of algorithm with minimum of several Quadratic forms
      function and gradient """
     np.random.seed(p)
     lambda_1 = 1
@@ -106,7 +299,7 @@ def test_3(p, d, num_points_t):
     assert(np.unique(pos_list).shape[0] == number_minimas)
 
 
-def test_4():
+def test_23():
     """Checks ouputs of algorithm with Sum of Gaussians function and
      gradient"""
     np.random.seed(11)
@@ -136,3 +329,66 @@ def test_4():
     assert(np.max(norms_with_minima) < 0.0001)
     """Ensure that each region of attraction discovered is unique"""
     assert(np.unique(pos_list).shape[0] == number_minimas)
+
+
+@settings(max_examples=10, deadline=None)
+@given(st.integers(2, 20), st.integers(1, 5), st.integers(2, 100))
+def test_24(p, m, d):
+    """Check that continued iterations from x_2 to a minimizer,
+    (iterations_of_sd_part), joined with the initial warm up points
+    (warm_up_sd), has the same points and shape compared to when
+    initial point x has steepest descent iterations applied.
+    """
+    beta = 0.099
+    tolerance = 0.00001
+    initial_guess = 0.05
+    projection = False
+    lambda_1 = 1
+    lambda_2 = 10
+    option = 'minimize'
+    met = 'Nelder-Mead'
+    f = mtv3.quad_function
+    g = mtv3.quad_gradient
+    """Create objective function parameters"""
+    store_x0, matrix_test = mtv3.function_parameters_quad(p, d, lambda_1,
+                                                          lambda_2)
+    func_args = p, store_x0, matrix_test
+    """Generate random starting point"""
+    bound_1 = 0
+    bound_2 = 1
+    x = np.random.uniform(bound_1, bound_2, (d, ))
+    warm_up_sd, warm_up_sd_partner_points = (mtv3.apply_sd_until_warm_up
+                                             (x, d, m, beta, projection,
+                                              option, met, initial_guess,
+                                              func_args, f, g, bound_1,
+                                              bound_2))
+    x_2 = warm_up_sd[m].reshape(d, )
+    iterations_of_sd_part, its = (mtv3.apply_sd_until_stopping_criteria
+                                  (x_2, d, projection, tolerance, option, met,
+                                   initial_guess, func_args, f, g, bound_1,
+                                   bound_2))
+    iterations_of_sd = np.vstack([warm_up_sd, iterations_of_sd_part[1:, ]]
+                                 )
+    sd_iterations_partner_points = (mtv3.partner_point_each_sd
+                                    (iterations_of_sd, d, beta, its + m, g,
+                                     func_args))
+    iterations_of_sd_test, its_test = (mtv3.apply_sd_until_stopping_criteria
+                                       (x, d, projection, tolerance, option,
+                                        met, initial_guess, func_args, f, g,
+                                        bound_1, bound_2))
+    sd_iterations_partner_points_test = (mtv3.partner_point_each_sd
+                                         (iterations_of_sd_test, d, beta,
+                                          its_test, g, func_args))
+
+    assert(np.all(np.round(iterations_of_sd_test, 4) == np.round
+           (iterations_of_sd, 4)))
+
+    assert(np.all(np.round(sd_iterations_partner_points_test, 4) == np.round
+           (sd_iterations_partner_points, 4)))
+
+    assert(iterations_of_sd_test.shape[0] == iterations_of_sd.shape[0])
+
+    assert(sd_iterations_partner_points_test.shape[0] ==
+           sd_iterations_partner_points.shape[0])
+
+    assert(its_test == its + m)
