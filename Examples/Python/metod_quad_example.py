cdb2a632ebc609b8e9a03b75176c5e76d38cf3b0
Meg Scammell <scammellm@cardiff.ac.uk>
2020-08-19 13:27:29 +0100

    Updating structure of METOD examples

--- Examples/Python/metod_quad_example.py
+++ Examples/Python/metod_quad_example.py
@@ -7,17 +7,18 @@ from metod import objective_functions as mt_obj
 
 """
 1) Define a function, gradient and dimension. Here the minimum of several
- quadratic forms function and gradient is used, and the dimension is 20.
+ quadratic forms function and gradient is used, and the dimension is 50.
 """
+
 f = mt_obj.quad_function
 g = mt_obj.quad_gradient
 d = 50
 
 """
-2) Will need to amend/add/delete the below if function and gradient is not
- the minimum of several quadratic forms. Otherwise, can update values for the
- below parameters
+2) Update values for the below parameters and seed number if required.
 """
+
+np.random.seed(90)
 P = 5
 lambda_1 = 1
 lambda_2 = 10
@@ -27,19 +28,23 @@ args = P, store_x0, matrix_combined
 
 """
 3) Run METOD algorithm. If any of the optional input parameters need to be
- updated this will need to be passed to mt.metod(f, g, args, d).
+ updated this will need to be passed to mt.metod(f, g, args, d). Update seed number if required.
 """
-discovered_minima, number_minima, func_vals_of_minima, excessive_no_descents = mt.metod(f, g, args, d)
+
+np.random.seed(91)
+(discovered_minima, number_minima,
+ func_vals_of_minima, excessive_no_descents) = mt.metod(f, g, args, d)
 np.savetxt('discovered_minimas_d_%s_p_%s.csv' % (d, P), discovered_minima,
            delimiter=",")
 
 """
 4) Save outputs from metod.py
 """
-np.savetxt('func_vals_discovered_minimas_d_%s_p_%s.csv' % (d, P),
+
+np.savetxt('func_vals_discovered_minimas_d_%s_p_%s_quad.csv' % (d, P),
            func_vals_of_minima, delimiter=",")
 
 summary_table = pd.DataFrame({
                              "Total number of unique minima": [number_minima],
                              "Extra descents": [excessive_no_descents]})
-summary_table.to_csv('summary_table_d_%s_p_%s.csv' % (d, P))
+summary_table.to_csv('summary_table_d_%s_p_%s_quad.csv' % (d, P))
---
+++ Examples/Python/metod_sog_example.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+
+import metod as mt
+from metod import objective_functions as mt_obj
+
+
+"""
+1) Define a function, gradient and dimension. Here the Sum of Gaussians function and gradient is used, and the dimension is 20.
+"""
+f = mtv3.sog_function
+g = mtv3.sog_gradient
+d = 100
+
+"""
+2) Update values for the below parameters and update seed number if required.
+Please note that sigma_sq needs to be carefully chosen, otherwise unexpected
+results may occur. Can select sigma_sq by trial and error.
+"""
+
+np.random.seed(90)
+P = 5
+lambda_1 = 1
+lambda_2 = 10
+sigma_sq = 4
+store_x0, matrix_combined, store_c = mtv3.function_parameters_sog(P, d, 
+                                                                  lambda_1, lambda_2)
+args = P, sigma_sq, store_x0, matrix_combined, store_c
+
+"""
+3) Run METOD algorithm. If any of the optional input parameters need to be
+ updated this will need to be passed to mt.metod(f, g, args, d). Update seed number if required.
+"""
+
+np.random.seed(91)
+(discovered_minima, number_minima,
+ func_vals_of_minima, excessive_no_descents) = mt.metod(f, g, args, d)
+np.savetxt('discovered_minimas_d_%s_p_%s.csv' % (d, P), discovered_minima,
+           delimiter=",")
+
+"""
+4) Save outputs from metod.py
+"""
+
+np.savetxt('func_vals_discovered_minimas_d_%s_p_%s_sog.csv' % (d, P),
+           func_vals_of_minima, delimiter=",")
+
+summary_table = pd.DataFrame({
+                             "Total number of unique minima": [number_minima],
+                             "Extra descents": [excessive_no_descents]})
+summary_table.to_csv('summary_table_d_%s_p_%s_sog.csv' % (d, P))
