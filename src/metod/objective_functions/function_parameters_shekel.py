e8b0f842e9c8aac756ca613e07d9a3efe25f2812
Meg Scammell <scammellm@cardiff.ac.uk>
2021-01-11 14:53:49 +0000

    Update function parameters for Shekel

--- src/metod/objective_functions/function_parameters_shekel.py
+++ src/metod/objective_functions/function_parameters_shekel.py
@@ -3,18 +3,12 @@ import numpy as np
 from metod import objective_functions as mt_obj
 
 
-def function_parameters_shekel(p, d, b_val, lambda_1, lambda_2):
+def function_parameters_shekel(lambda_1, lambda_2):
     """
     Create function arguments for the Sum of Gaussians function and gradient.
 
     Parameters
     ----------
-    p : integer
-        Number of local minima.
-    d : integer
-        Size of dimension.
-    b_val : integer or float
-            Value of each element of array b.
     lambda_1 : integer
                Smallest eigenvalue of diagonal matrix.
     lambda_2 : integer
@@ -26,29 +20,24 @@ def function_parameters_shekel(p, d, b_val, lambda_1, lambda_2):
                   are generated from
                   np.random.uniform(lambda_1 + 0.1,
                                     lambda_2 - 0.1).
-    C : 2-D arrays with shape (d, p). Entries are
-        generated from np.random.uniform(0, 1).
-    B : 1-D array with shape (p, ). Each entry is b_val.
+    C : 2-D arrays with shape (4, 10).
+    B : 1-D array with shape (10, ).
     """
-    if type(p) is not int:
-        raise ValueError('p must be an integer.')
-    if type(d) is not int:
-        raise ValueError('d must be an integer.')
+    d = 4
+    p = 10
     if type(lambda_1) is not int:
         raise ValueError('lambda_1 must be an integer.')
     if type(lambda_2) is not int:
         raise ValueError('lambda_2 must be an integer.')
-    if (type(b_val) is not int) and (type(b_val) is not float):
-        raise ValueError('b_val must be an integer.')
-    C = np.zeros((d, p))
-    b = np.zeros((p))
+    C = np.array([[4, 1, 8, 6, 3, 2, 5, 8, 6, 7],
+                  [4, 1, 8, 6, 7, 9, 3, 1, 2, 3.6],
+                  [4, 1, 8, 6, 3, 2, 5, 8, 6, 7],
+                  [4, 1, 8, 6, 7, 9, 3, 1, 2, 3.6]])
+    b = np.array([0.1, 0.2, 0.2, 0.4, 0.4, 0.6, 0.3, 0.7, 0.5, 0.5])
     A = np.zeros((p, d, d))
     rotation = np.zeros((p, d, d))
-
     for j in range(p):
-        C[:, j] = np.random.uniform(0, 1, (d)) 
         rotation[j] = mt_obj.calculate_rotation_matrix(d, 3)
-        b[j] = b_val
         diag_vals = np.zeros(d)
         diag_vals[:2] = np.array([lambda_1, lambda_2])
         diag_vals[2:] = np.random.uniform(lambda_1 + 0.1,
