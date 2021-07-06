0f7d681881e8b41466961b3fe34663c7a3b4814e
Meg Scammell <scammellm@cardiff.ac.uk>
2021-07-06 17:19:59 +0100

    Update METOD numerical examples

--- Numerical_Experiments/METOD_algorithm_numerical_examples/metod_numerical_exp_boxplots.py
+++ Numerical_Experiments/METOD_algorithm_numerical_examples/metod_numerical_exp_boxplots.py
@@ -15,20 +15,41 @@ def data_boxplots(func_name, d, p, num_p, beta_list, m_list, set_x, option,
                   initial_guess):
 
     avg_grad = np.zeros((len(m_list) * len(beta_list), 100, num_p))
+    grad_evals_metod = np.zeros((len(m_list) * len(beta_list), 100, num_p))
+    sp_metod = np.zeros((len(m_list) * len(beta_list), 100 * num_p, d))
     total_no_local_minimizers_metod = np.zeros((len(m_list), len(beta_list), 100))
     time_taken_metod = np.zeros((len(m_list), len(beta_list), 100))
     extra_descents_metod = np.zeros((len(m_list), len(beta_list), 100))
     func_val_metod = np.zeros((len(m_list), len(beta_list), 100))
 
     if p is not None and sigma_sq is not None:
-        df_mult = pd.read_csv('%s_sd_metod_beta_0.1_m=1_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv' %
+        df_mult = pd.read_csv('%s_sd_metod_beta_0.1_m=2_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv' %
                                     (func_name, d, p, set_x, sigma_sq, num_p, option, initial_guess))
+        grad_evals_mult = np.genfromtxt('%s_grad_evals_mult_beta_0.1_m=2_d=%sp=%s_%s_sig_%s_%s_%s_%s.csv'%
+                                        (func_name, d, p, set_x, sigma_sq, num_p, option, initial_guess),
+                                         delimiter=',')
+        sp_mult = np.genfromtxt('%s_sd_start_p_beta_0.1_m=2_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv'%
+                                (func_name, d, p, set_x, sigma_sq, num_p, option, initial_guess),
+                                 delimiter=',')
     elif p is not None:
-        df_mult = pd.read_csv('%s_sd_metod_beta_0.1_m=1_d=%s_p=%s_%s_%s_%s_%s.csv' %
+        df_mult = pd.read_csv('%s_sd_metod_beta_0.1_m=2_d=%s_p=%s_%s_%s_%s_%s.csv' %
                                     (func_name, d, p, set_x, num_p, option, initial_guess))
+        grad_evals_mult = np.genfromtxt('%s_grad_evals_mult_beta_0.1_m=2_d=%s_p=%s_%s_%s_%s_%s.csv'%
+                                        (func_name, d, p, set_x, num_p, option, initial_guess),
+                                         delimiter=',')
+        sp_mult = np.genfromtxt('%s_sd_start_p_beta_0.1_m=2_d=%s_p=%s_%s_%s_%s_%s.csv'%
+                                (func_name, d, p, set_x, num_p, option, initial_guess),
+                                 delimiter=',')
     else:
-        df_mult = pd.read_csv('%s_sd_metod_beta_0.1_m=1_d=%s_%s_%s_%s_%s.csv' %
+        df_mult = pd.read_csv('%s_sd_metod_beta_0.001_m=2_d=%s_%s_%s_%s_%s.csv' %
                                     (func_name, d, set_x, num_p, option, initial_guess))
+        grad_evals_mult = np.genfromtxt('%s_grad_evals_mult_beta_0.001_m=2_d=%s_%s_%s_%s_%s.csv'%
+                                        (func_name, d, set_x, num_p, option, initial_guess),
+                                         delimiter=',')
+        sp_mult = np.genfromtxt('%s_sd_start_p_beta_0.001_m=2_d=%s_%s_%s_%s_%s.csv'%
+                                (func_name, d, set_x, num_p, option, initial_guess),
+                                 delimiter=',')
+                                
     total_no_local_minimizers_mult = np.array(df_mult['number_minimizers_per_func_multistart'])
     time_taken_mult = np.array(df_mult['time_multistart'])
     func_val_mult = np.array(df_mult['min_func_val_multistart'])
@@ -59,26 +80,49 @@ def data_boxplots(func_name, d, p, num_p, beta_list, m_list, set_x, option,
                 avg_grad[index_all] = np.genfromtxt('%s_grad_norm_beta_%s_m=%s_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv'%
                                                     (func_name,beta, m, d, p, set_x, sigma_sq, num_p, option, initial_guess),
                                                      delimiter=',')
+                grad_evals_metod[index_all] = np.genfromtxt('%s_grad_evals_metod_beta_%s_m=%s_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv'%
+                                                            (func_name, beta, m, d, p, set_x, sigma_sq, num_p, option, initial_guess),
+                                                             delimiter=',')
+                sp_metod[index_all] = np.genfromtxt('%s_start_p_beta_%s_m=%s_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv'%
+                                                    (func_name, beta, m, d, p, set_x, sigma_sq, num_p, option, initial_guess),
+                                                     delimiter=',')
             elif p is not None:
                 avg_grad[index_all] = np.genfromtxt('%s_grad_norm_beta_%s_m=%s_d=%s_p=%s_%s_%s_%s_%s.csv'%
                                                     (func_name,beta, m, d, p, set_x, num_p, option, initial_guess),
+                                                     delimiter=',')
+                grad_evals_metod[index_all] = np.genfromtxt('%s_grad_evals_metod_beta_%s_m=%s_d=%s_p=%s_%s_%s_%s_%s.csv'%
+                                                            (func_name,beta, m, d, p, set_x, num_p, option, initial_guess),
+                                                             delimiter=',')
+                sp_metod[index_all] = np.genfromtxt('%s_start_p_beta_%s_m=%s_d=%s_p=%s_%s_%s_%s_%s.csv'%
+                                                    (func_name, beta, m, d, p, set_x, num_p, option, initial_guess),
                                                      delimiter=',')
             else:
                 avg_grad[index_all] = np.genfromtxt('%s_grad_norm_beta_%s_m=%s_d=%s_%s_%s_%s_%s.csv'%
                                                     (func_name,beta, m, d, set_x, num_p, option, initial_guess),
                                                      delimiter=',')
+                grad_evals_metod[index_all] = np.genfromtxt('%s_grad_evals_metod_beta_%s_m=%s_d=%s_%s_%s_%s_%s.csv'%
+                                                            (func_name,beta, m, d, set_x, num_p, option, initial_guess),
+                                                             delimiter=',')
+                sp_metod[index_all] = np.genfromtxt('%s_start_p_beta_%s_m=%s_d=%s_%s_%s_%s_%s.csv'%
+                                                    (func_name, beta, m, d, set_x, num_p, option, initial_guess),
+                                                     delimiter=',')
             index_beta += 1
             index_all += 1
         index_m += 1
+
+    check_avg_grad_and_sp_points(avg_grad, beta_list, m_list, sp_metod, sp_mult)
+
     return (total_no_local_minimizers_metod, total_no_local_minimizers_mult,
             time_taken_metod, time_taken_mult, func_val_metod, func_val_mult,
-            extra_descents_metod, avg_grad)
+            extra_descents_metod, avg_grad, grad_evals_metod, grad_evals_mult)
 
 
-def check_avg_grad(avg_grad, beta_list, m_list):
+def check_avg_grad_and_sp_points(avg_grad, beta_list, m_list, sp_metod, sp_mult):
     for j in range(len(beta_list) * len(m_list)):
         for k in range(j+1, len(beta_list) * len(m_list)):
-            assert(np.all(np.round(avg_grad[k], 5) == np.round(avg_grad[j], 5)))
+                assert(np.all(np.round(avg_grad[k], 5) == np.round(avg_grad[j], 5)))
+                assert(np.all(np.round(sp_metod[k], 5) == np.round(sp_metod[j], 5)))
+                assert(np.all(np.round(sp_mult, 5) == np.round(sp_metod[j], 5)))
 
 
 def write_to_latex(arr, func_name, title, d, num_p, set_x,
@@ -95,39 +139,45 @@ def write_to_latex(arr, func_name, title, d, num_p, set_x,
 
 def compute_tables(m_list, beta_list, total_no_local_minimizers_mult,
                    total_no_local_minimizers_metod, func_val_mult,
-                   func_val_metod, time_taken_mult, time_taken_metod,
-                   extra_descents_metod):
+                   func_val_metod):
     same_global_min = np.zeros((len(m_list), len(beta_list)))
-    same_minimizer = np.zeros((len(m_list), len(beta_list)))
-    time_taken_overall = np.zeros((len(m_list), len(beta_list)))
+    minimizers_same = np.zeros((len(m_list), len(beta_list), 100))
     for i in range(len(m_list)):
         for j in range(len(beta_list)):
             assert(np.all(total_no_local_minimizers_mult >=  total_no_local_minimizers_metod[i, j]))
             same_global_min[i, j] = np.where(np.round(func_val_mult, 2) == np.round(func_val_metod[i, j], 2))[0].shape[0]/100
-            same_minimizer[i, j] = np.sum(total_no_local_minimizers_metod[i, j]) / np.sum(total_no_local_minimizers_mult)
-            time_taken_overall[i, j] = np.sum(time_taken_metod[i, j]) / np.sum(time_taken_mult)
-    
+            minimizers_same[i, j] = total_no_local_minimizers_mult - total_no_local_minimizers_metod[i,j]
+
+    compute_freq_minimizers_found = np.zeros((len(m_list) * len(beta_list), 4))
+    index = 0
+    for i in range(len(m_list)):
+        for j in range(len(beta_list)):
+            compute_freq_minimizers_found[index, 0] = np.where(minimizers_same[i, j] == 0)[0].shape[0]
+            compute_freq_minimizers_found[index, 1] = np.where(minimizers_same[i, j] == 1)[0].shape[0]
+            compute_freq_minimizers_found[index, 2] = np.where(minimizers_same[i, j] >= 2)[0].shape[0]
+            compute_freq_minimizers_found[index, 3] = same_global_min[i, j]
+            index += 1
+
+    write_to_latex(compute_freq_minimizers_found, func_name, 'freq_minimizers', d, num_p, set_x,
+                   option, initial_guess)
+
     write_to_latex(np.round(same_global_min, 3), func_name, 'global_min', d, num_p, set_x,
-                   option, initial_guess)
-    write_to_latex(np.round(same_minimizer, 3), func_name, 'same_minimizer', d, num_p, set_x,
-                   option, initial_guess)
-    write_to_latex(np.round(time_taken_overall, 3), func_name, 'time_taken_overall', d, num_p, set_x,
                    option, initial_guess)
 
 
 def function_info(func_name, d, p, set_x, sigma_sq, num_p, option, avg_grad):
     if p is not None and sigma_sq is not None:
-        number_its = np.genfromtxt('%s_no_its_mult_beta_0.1_m=1_d=%sp=%s_%s_sig_%s_%s_%s_%s.csv' %
-                                 (func_name, d, p, set_x, sigma_sq, num_p, option, initial_guess),
+        number_its = np.genfromtxt('%s_grad_evals_mult_beta_0.1_m=2_d=%s_p=%s_%s_sig_%s_%s_%s_%s.csv' %
+                                   (func_name, d, p, set_x, sigma_sq, num_p, option, initial_guess),
                                     delimiter=',')
     elif p is not None:
-        number_its = np.genfromtxt('%s_no_its_mult_beta_0.1_m=1_d=%sp=%s_%s_%s_%s_%s.csv' %
+        number_its = np.genfromtxt('%s_grad_evals_mult_beta_0.1_m=2_d=%s_p=%s_%s_%s_%s_%s.csv' %
                                    (func_name, d, p, set_x, num_p, option, initial_guess),
-                                    delimiter=',')
+                                    delimiter=',') - 1
     else:
-        number_its = np.genfromtxt('%s_no_its_mult_beta_0.1_m=1_d=%s_%s_%s_%s_%s.csv' %
+        number_its = np.genfromtxt('%s_grad_evals_mult_beta_0.001_m=2_d=%s_%s_%s_%s_%s.csv' %
                                    (func_name, d, set_x, num_p, option, initial_guess),
-                                    delimiter=',')
+                                    delimiter=',') - 1
     store_mean_its = np.zeros((100))
     store_min_its = np.zeros((100))
     store_max_its = np.zeros((100))
@@ -153,6 +203,17 @@ def set_box_color(bp, color):
     plt.setp(bp['medians'], color=color)
 
 
+def compute_prop_grad_evals(m_list, beta_list, grad_evals_metod, grad_evals_mult):
+    index_all = 0
+    prop_grad_evals = np.zeros((len(m_list), len(beta_list), 100))
+    for j in range(len(m_list)):
+        for i in range(len(beta_list)):
+            for func in range(100):
+                prop_grad_evals[j, i, func] = np.sum(grad_evals_metod[index_all, func]) / np.sum(grad_evals_mult[func])
+            index_all += 1
+    return prop_grad_evals
+
+
 def create_boxplots_ratio_m_1(arr1, labels, ticks, func_name, title, d, num_p,
                               set_x, p, sigma_sq, option, initial_guess):
     plt.figure(figsize=(7, 5))
@@ -170,15 +231,15 @@ def create_boxplots_ratio_m_1(arr1, labels, ticks, func_name, title, d, num_p,
     plt.tight_layout()
 
     if p is not None and sigma_sq is not None:
-        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_sig=%s_%s_%s.pdf' %
+        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_sig=%s_%s_%s.png' %
                     (func_name, title, d, num_p, set_x,
                      p, sigma_sq, option, initial_guess))
     elif p is not None:
-        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_%s_%s.pdf' %
+        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_%s_%s.png' %
                     (func_name, title, d, num_p, set_x,
                      p, option, initial_guess))
     else:
-        plt.savefig('%s_%s_d=%s_%s_%s_%s_%s.pdf' %
+        plt.savefig('%s_%s_d=%s_%s_%s_%s_%s.png' %
                     (func_name, title, d, num_p, set_x,
                      option, initial_guess))
 
@@ -206,29 +267,29 @@ def create_boxplots_ratio_m_2(arr1, arr2, labels, ticks, func_name, title, d, nu
     plt.tight_layout()
 
     if p is not None and sigma_sq is not None:
-        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_sig=%s_%s_%s.pdf' %
+        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_sig=%s_%s_%s.png' %
                     (func_name, title, d, num_p, set_x,
                      p, sigma_sq, option, initial_guess))
     elif p is not None:
-        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_%s_%s.pdf' %
+        plt.savefig('%s_%s_d=%s_%s_%s_p=%s_%s_%s.png' %
                     (func_name, title, d, num_p, set_x,
                      p, option, initial_guess))
     else:
-        plt.savefig('%s_%s_d=%s_%s_%s_%s_%s.pdf' %
+        plt.savefig('%s_%s_d=%s_%s_%s_%s_%s.png' %
                     (func_name, title, d, num_p, set_x,
                      option, initial_guess))
 
 
 if __name__ == "__main__":
-    func_name = 'quad'
-    d = 100
-    num_p = 1000
-    beta_list = [0.1, 0.01, 0.001]
+    func_name = 'shekel'
+    d = 4
+    num_p = 100
+    beta_list = [0.01, 0.1, 0.2]
     m_list = [2, 3]
     set_x = 'random'
-    p = 50
+    p = 10
     sigma_sq = None
-    option = 'f'
+    option = 'm'
     initial_guess = 0.005
 
     (total_no_local_minimizers_metod,
@@ -238,25 +299,29 @@ if __name__ == "__main__":
      func_val_metod,
      func_val_mult,
      extra_descents_metod,
-     avg_grad) = data_boxplots(func_name, d, p, num_p, beta_list, m_list,
-                               set_x, option, initial_guess)
+     avg_grad,
+     grad_evals_metod,
+     grad_evals_mult) = data_boxplots(func_name, d, p, num_p, beta_list, m_list,
+                                       set_x, option, initial_guess)
 
     total_no_minimizers_prop = (total_no_local_minimizers_metod /
                                 total_no_local_minimizers_mult)
 
     time_taken_prop = time_taken_metod / time_taken_mult
 
-    extra_descents_prop = (extra_descents_metod /
-                           (num_p - total_no_local_minimizers_metod))
-    check_avg_grad(avg_grad, beta_list, m_list)
+    extra_descents_prop = ((extra_descents_metod +  total_no_local_minimizers_metod)/
+                           (num_p))
+
+    grad_evals_prop = compute_prop_grad_evals(m_list, beta_list,
+                                              grad_evals_metod,
+                                              grad_evals_mult)
 
 
     function_info(func_name, d, p, set_x, sigma_sq, num_p, option, avg_grad)
 
     compute_tables(m_list, beta_list, total_no_local_minimizers_mult,
                    total_no_local_minimizers_metod, func_val_mult,
-                   func_val_metod, time_taken_mult, time_taken_metod,
-                   extra_descents_metod)
+                   func_val_metod)
 
     ticks = []
     labels = []
@@ -270,21 +335,28 @@ if __name__ == "__main__":
                                   func_name, 'no_minimizers_prop', d, num_p, set_x, p, sigma_sq,
                                   option, initial_guess)
 
-        create_boxplots_ratio_m_1(time_taken_prop[0], labels, ticks,
-                                  func_name, 'time_prop', d, num_p, set_x, p, sigma_sq,
-                                  option, initial_guess)
+        # create_boxplots_ratio_m_1(time_taken_prop[0], labels, ticks,
+        #                           func_name, 'time_prop', d, num_p, set_x, p, sigma_sq,
+        #                           option, initial_guess)
         create_boxplots_ratio_m_1(extra_descents_prop[0], labels, ticks,
                                   func_name, 'ex_des_prop', d, num_p, set_x, p, sigma_sq,
+                                  option, initial_guess)
+        create_boxplots_ratio_m_1(grad_evals_prop[0], labels, ticks,
+                                  func_name, 'grad_evals_prop', d, num_p, set_x, p, sigma_sq,
                                   option, initial_guess)
     else:
         create_boxplots_ratio_m_2(total_no_minimizers_prop[0],
                                   total_no_minimizers_prop[1], labels, ticks,
                                   func_name, 'no_minimizers_prop', d, num_p, set_x, p, sigma_sq,
                                   option, initial_guess)
-        create_boxplots_ratio_m_2(time_taken_prop[0], time_taken_prop[1], labels, ticks,
-                                  func_name, 'time_prop', d, num_p, set_x, p, sigma_sq,
-                                  option, initial_guess)
+        # create_boxplots_ratio_m_2(time_taken_prop[0], time_taken_prop[1], labels, ticks,
+        #                           func_name, 'time_prop', d, num_p, set_x, p, sigma_sq,
+        #                           option, initial_guess)
         create_boxplots_ratio_m_2(extra_descents_prop[0],
                                   extra_descents_prop[1], labels, ticks,
                                   func_name, 'ex_des_prop', d, num_p, set_x, p, sigma_sq,
+                                  option, initial_guess)
+        create_boxplots_ratio_m_2(grad_evals_prop[0],
+                                  grad_evals_prop[1], labels, ticks,
+                                  func_name, 'grad_evals_prop', d, num_p, set_x, p, sigma_sq,
                                   option, initial_guess)
