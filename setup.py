8c0b2871450dafe64b1ed1ec75b15f3c28dd31bb
Meg Scammell <scammellm@cardiff.ac.uk>
2021-07-14 16:26:04 +0100

    Update file

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ requirements = ["numpy>=1.16.2", "scipy>=1.2.1", "pytest>=4.3.1",
                 "tqdm>=4.32.1", "pandas>=0.24.2", "setuptools>=42.0.2",
                 "hypothesis>=5.1.5 ", "SALib>=1.3.12"]
 
-with open("README.MD", "r") as f:
-    long_description = f.read()
+# with open("README.MD", "r") as f:
+#     long_description = f.read()
 
 setup(
     name="metod_alg",
@@ -21,7 +21,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"":"src"},
     description="Applying METOD algorithm for a number of starting points.",
-    long_description=long_description,
-    long_description_content_type="text/x-MD",
+    # long_description=long_description,
+    # long_description_content_type="text/x-MD",
 )
 
